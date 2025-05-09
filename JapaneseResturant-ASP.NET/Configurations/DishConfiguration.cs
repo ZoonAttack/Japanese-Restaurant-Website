baf192028168fd644293efea5e96730477163983
Ali Mohamed <ali.elsabbagh@ejust.edu.eg>
2025-05-09 14:27:02 +0300

    EF Model Setup #2

---
+++ JapaneseResturant-ASP.NET/Configurations/DishConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using JapaneseRestaurantModel.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JapaneseRestaurantModel.Configurations
+{
+    public class DishConfiguration : IEntityTypeConfiguration<Dish>
+    {
+        void IEntityTypeConfiguration<Dish>.Configure(EntityTypeBuilder<Dish> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+
+            builder.Property(x => x.Name).HasColumnType("VARCHAR").HasMaxLength(100).IsRequired();
+            builder.Property(x=>x.Price).HasPrecision(5, 2);
+            builder.Property(x => x.Description).HasColumnType("TEXT").IsRequired();
+            builder.Property(x => x.Category).HasColumnType("VARCHAR").HasMaxLength(100).IsRequired();
+            builder.Property(x=>x.PictureURL).HasColumnType("VARCHAR").HasMaxLength(255).IsRequired();
+
+            builder.HasData(LoadDishs());
+        }
+
+        private static List<Dish> LoadDishs()
+        {
+            return new List<Dish>
+            {
+
+            };
+        }
+    }
+}
---
+++ JapaneseResturant-ASP.NET/Configurations/ReviewConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using JapaneseRestaurantModel.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JapaneseRestaurantModel.Configurations
+{
+    public class ReviewConfiguration : IEntityTypeConfiguration<Review>
+    {
+        void IEntityTypeConfiguration<Review>.Configure(EntityTypeBuilder<Review> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+
+            builder.Property(x => x.Comment).HasColumnType("TEXT");
+            builder.Property(x => x.Rating).IsRequired();
+
+            // Dish <-1---N=> Review
+            builder.HasOne(x => x.Dish)
+                .WithMany(x => x.Reviews)
+                .HasForeignKey(x => x.DishId)
+                .IsRequired();
+
+            // Customer <-1---N=> Review
+            builder.HasOne(x => x.Customer)
+                .WithMany(x => x.Reviews)
+                .HasForeignKey(x => x.CustomerId)
+                .IsRequired();
+        }
+    }
+}
